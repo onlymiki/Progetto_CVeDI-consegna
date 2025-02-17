86fb98ad779503e469335a517346619c84623b92
onlyMiki <michela.mariani01@gmail.com>
2025-02-17 17:49:05 +0100

    Aggiornamento standby

--- js/standby.js
+++ js/standby.js
@@ -1,4 +1,14 @@
 // ---------- Collegamento con la Home  ----------
-document.addEventListener("click", function() {
-    window.location.href = "/index-home.html"; 
+document.querySelectorAll(".touch").forEach(function (element) {
+    element.addEventListener("click", function () {
+        window.location.href = "/index-home.html";
+    });
 });
+
+
+
+// ---------- SOS ----------
+const sos = document.getElementById("sos");
+function showSos() {
+    sos.classList.toggle("d-none");
+}
